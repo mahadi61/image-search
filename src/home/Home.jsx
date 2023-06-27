63749f76e66291b8b1a581e4b2d8f4592dae35a2
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-06-27 16:50:58 +0600

    t

---
+++ src/home/Home.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { BeatLoader } from "react-spinners";
+import SingleImage from "./SingleImage";
+
+const Home = () => {
+  const [images, setImages] = useState([]);
+  // const [name, setName] = useState("");
+  const [loader, setLoader] = useState(true);
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    // setName(name);
+    fetch(
+      `https://pixabay.com/api/?key=37751582-2d5c45858694200b38b39ca6c&q=${name}&image_type=photo&pretty=true`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setLoader(false);
+        setImages(data.hits);
+      });
+  };
+
+  return (
+    <div>
+      <h1 className="text-center text-sky-500 capitalize text-4xl mt-4">
+        search image
+      </h1>
+      <form onSubmit={handleSearch} className="text-center my-5">
+        <input
+          type="text"
+          name="name"
+          className="border border-blue-800 p-2 rounded-lg me-3"
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="px-4 py-2 border border-sky-800 rounded-lg"
+        />
+      </form>
+      {loader && (
+        <div className="text-center mt-5">
+          <BeatLoader color="#36d7b7" />
+        </div>
+      )}
+      {/* {console.log(images === [])} */}
+      <div className="grid grid-cols-4 gap-4">
+        {images.map((image, i) => (
+          <SingleImage key={i} image={image}></SingleImage>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/home/SingleImage.jsx
@@ -0,0 +1,9 @@
+const SingleImage = ({ image }) => {
+  return (
+    <div className="">
+      <img className="" src={image.largeImageURL} alt="" />
+    </div>
+  );
+};
+
+export default SingleImage;
